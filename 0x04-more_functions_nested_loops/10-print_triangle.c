dd525b0d4aaddfd7873a5a4a9fa7cfcf633d86e4
Tori-Bemide <tori12timony@gmail.com>
2023-09-14 19:36:39 -0700

    added 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,16 +8,21 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 0; j < (size - i - 1); j++)
+		for (i = 0; i < size; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < (size - i - 1); j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i + 1; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		for (k = 0; k < i + 1; k++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
 	}
+	else
+		_putchar('\n');
 }
