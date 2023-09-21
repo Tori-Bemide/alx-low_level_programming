fd040dbd7acce78614fc3896c3601803d5b7d5b3
Tori-Bemide <tori12timony@gmail.com>
2023-09-21 13:52:01 -0700

    added 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - function is similar to the _strcat function
+ *
+ * @dest: first string
+ *
+ * @src: second string
+ *
+ * @n: most bytes from src
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, len = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		len += 1;
+
+	for (j = 0; j < n; j++)
+		dest[len + j] = src[j];
+
+	return (dest);
+}
