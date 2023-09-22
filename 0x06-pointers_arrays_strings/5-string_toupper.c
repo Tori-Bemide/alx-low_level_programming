a12218f6a91c6605e6015b25317f6f6706c967ce
Tori-Bemide <tori12timony@gmail.com>
2023-09-22 04:33:31 -0700

    added 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters
+ * of a string to uppercase
+ *
+ * @a: string to be converted
+ *
+ * Return: a
+ */
+
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if ((a[i] >= 97) && (a[i] <= 122))
+		{
+			a[i] = a[i] - 32;
+		}
+	}
+	return (a);
+}
