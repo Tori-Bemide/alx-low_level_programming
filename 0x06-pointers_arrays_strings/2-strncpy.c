44038d5722c320de9ba83c4797121ea8803b1570
Tori-Bemide <tori12timony@gmail.com>
2023-09-22 00:15:58 -0700

    added 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main'h"
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: first string
+ *
+ * @src: second string
+ *
+ * @n: no of bytes to be copies from src
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[j] = '\0';
+
+	return (dest);
+}
