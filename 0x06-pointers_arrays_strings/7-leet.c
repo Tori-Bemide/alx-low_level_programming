ec347d70fc32a7bbcaa378367d61b08d3ea417f0
Tori-Bemide <tori12timony@gmail.com>
2023-09-22 18:39:10 -0700

    added 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ *
+ * @a: string
+ *
+ * Return: a
+ */
+
+char *leet(char *a)
+{
+	char *b = a;
+
+	while (*b)
+	{
+		char *a_chars = "AaEeOoTtLl";
+		char *a_replacements = "4433007711";
+
+		for (int i = 0; a_chars[i]; i++)
+		{
+			if (*b == a_chars[i])
+			{
+				*b = a_replacements[i];
+				break;
+			}
+		}
+
+		b++;
+	}
+
+	return (a);
+}
