aff22446822735ff7e0585cd1215c4ac5c1ee2ef
Tori-Bemide <tori12timony@gmail.com>
2023-09-25 17:59:55 -0700

    added 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: first string
+ *
+ * @accept: second string
+ *
+ * Return: count
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *a;
+	int found;
+
+	while (*s)
+	{
+		char *a = accept;
+		int found = 0;
+
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				found = 1;
+				break;
+			}
+			a++;
+		}
+
+		if (!found)
+		{
+			break;
+		}
+
+		count++;
+		s++;
+	}
+
+	return (count);
+}
