c01a97891416c93b1b65a1a592ded68ad0b9df40
Tori-Bemide <tori12timony@gmail.com>
2023-09-27 00:53:07 -0700

    added 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -23,5 +22,5 @@ char *_strchr(char *s, char c)
 		}
 	}
 
-	return (NULL);
+	return ('\0');
 }
