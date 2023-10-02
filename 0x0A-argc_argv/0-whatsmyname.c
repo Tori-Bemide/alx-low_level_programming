7ac42b575b1fab06ac445afd7b2a61f1338eb45e
Tori-Bemide <tori12timony@gmail.com>
2023-10-02 13:25:46 -0700

    added 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * whatsmyname - a program that prints its name, followed by a new line
+ *
+ * @argc: unused variable
+ *
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int whatsmyname(int argc, char *argv[])
+{
+	printf("%c", argv[0];
+	return (0);
+}
