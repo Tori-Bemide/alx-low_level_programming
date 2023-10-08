ff4f5d882108eab142456029568c278d87c500a4
Tori-Bemide <tori12timony@gmail.com>
2023-10-08 13:18:08 -0700

    added 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,21 +13,21 @@
 
 int *array_range(int min, int max)
 {
-	int i, j, len, *a;
+	int i, j = min, len = (max - min) + 1, *a;
 
 	if (min > max)
 		return (NULL);
 
-	for (i = min; i <= max; i++)
-		len++;
+	a = (int *)malloc(sizeof(int) * len);
 
-	a = (int *)malloc((sizeof(int) * len) + 1);
-	
 	if (a == NULL)
 		return (NULL);
-	
-	for (i = 0, j = min; j <= max; i++, j++)
+
+	for (i = 0; i < len; i++)
+	{
 		a[i] = j;
+		j++;
+	}
 
 	a[i] = '\0';
 
