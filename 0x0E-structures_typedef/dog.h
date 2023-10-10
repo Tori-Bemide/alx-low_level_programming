c681da1a1eac85cc9e5c451dbac7451a7df2bd81
Tori-Bemide <tori12timony@gmail.com>
2023-10-10 13:11:05 -0700

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure
+ *
+ * @name: first member
+ *
+ * @age: second member
+ *
+ * @owner: third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
